b3f6ff637973db2a4784c06c596ca559c6a8a347
victorjatoba <victorjatoba10@gmail.com>
2018-04-05 22:24:48 -0300

    Plotar gráfico: ISRs performance from all thetas (th) x Range of thetas level

--- experiment/ploting-isr-comparative.R
+++ experiment/ploting-isr-comparative.R
@@ -7,10 +7,10 @@
 
 
 #choice the theta level range you want to plot
-#thetaLevel <- "high"
+thetaLevel <- "high"
 #thetaLevel <- "medium"
 #thetaLevel <- "low"
-thetaLevel <- "all"
+#thetaLevel <- "all"
 g_range <- matrix()
 
 if (thetaLevel == "high") {
@@ -46,11 +46,11 @@ if (thetaLevel == "high") {
 step <- 0.1
 package <- "5k_examinees/2012"
 mfiResult  <- getRangeByItemsMean("MFI", package, initValue, stopValue, step)[,1]
-#klResult   <- getRangeByItemsMean("KL", package, initValue, stopValue, step)[,1]
-#klpResult  <- getRangeByItemsMean("KLP", package, initValue, stopValue, step)[,1]
-#mpwiResult <- getRangeByItemsMean("MPWI", package, initValue, stopValue, step)[,1]
-#gdiResult <- getRangeByItemsMean("GDI", package, initValue, stopValue, step)[,1]
-#gdipResult <- getRangeByItemsMean("GDIP", package, initValue, stopValue, step)[,1]
+klResult   <- getRangeByItemsMean("KL", package, initValue, stopValue, step)[,1]
+klpResult  <- getRangeByItemsMean("KLP", package, initValue, stopValue, step)[,1]
+mpwiResult <- getRangeByItemsMean("MPWI", package, initValue, stopValue, step)[,1]
+gdiResult <- getRangeByItemsMean("GDI", package, initValue, stopValue, step)[,1]
+gdipResult <- getRangeByItemsMean("GDIP", package, initValue, stopValue, step)[,1]
 
 plot(mfiResult, type = "o",
      ylim=g_range,
@@ -96,21 +96,21 @@ axis(2, cex.axis=0.7, las=2, at=1*0:(g_range[2]))
 box()
 
 # Graph KL with red dashed line and square points
-#lines(klResult, type="o", pch=4, lty=2, col="green")
+lines(klResult, type="o", pch=4, lty=2, col="green")
 # Graph KLP with green dashed line and square points
-#lines(klpResult, type="o", pch=5, lty=4, col="darkgreen")
+lines(klpResult, type="o", pch=5, lty=4, col="darkgreen")
 # Graph MPWI with purple dashed line and square points
-#lines(mpwiResult, type="o", pch=10, lty=6, col="purple")
+lines(mpwiResult, type="o", pch=10, lty=6, col="purple")
 # Graph MPWI with purple dashed line and square points
-#lines(gdiResult, type="o", pch=15, lty=15, col="red")
+lines(gdiResult, type="o", pch=15, lty=15, col="red")
 # Graph MPWI with purple dashed line and square points
-#lines(gdipResult, type="o", pch=20, lty=18, col="orange")
+lines(gdipResult, type="o", pch=20, lty=18, col="orange")
 
 # Create a legend at (1, g_range[2]) that is slightly smaller 
 # (cex) and uses the same line colors and points used by 
 # the actual plots 
-legend('bottomleft',g_range[2], c("MFI", "KLP", "MPWI",  "GDIP"), cex=0.6, 
-       col=c("blue", "darkgreen", "purple", "orange"), pch=21:22, lty=1:2)
+legend('bottomleft',g_range[2], c("MFI", "KL", "KLP", "MPWI",  "GDI", "GDIP"), cex=0.3, 
+       col=c("blue", "green", "darkgreen", "purple", "red", "orange"), pch=11:12, lty=1:2)
 
 title(main=paste("ISRs performance from ", thetaLevel ," thetas (th)", sep = ""), sub="[th, th[",
       ylab="selected items quantities", xlab="Range of thetas level") 
